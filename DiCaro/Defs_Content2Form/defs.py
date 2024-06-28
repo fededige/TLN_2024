c9d3712266baf529f4fede9358cf1454b367fc69
Federico <fededigeronimo@gmail.com>
2024-06-28 16:19:40 +0200

    comment added to defs

--- DiCaro/Defs_Content2Form/defs.py
+++ DiCaro/Defs_Content2Form/defs.py
@@ -1,21 +1,8 @@
 import string
-from nltk import SnowballStemmer
 from nltk.corpus import stopwords
 
-
-# def get_similarity(defs):
-#     word_in_common = set([snow_stm.stem(word) for word in defs[0].split(' ') if word not in stopword])
-#     min_len = len(defs[0])
-#     i = 0
-#     for definition in defs:
-#         words = [snow_stm.stem(word) for word in definition.split(' ') if word not in stopword]
-#         if len(words) > 0:
-#             word_in_common = word_in_common.intersection(words)
-#             if len(definition) < min_len:
-#                 min_len = len(words)
-#     return len(word_in_common) / min_len
-
-
+# Calcolo delle frequenze delle parole all'interno di defs.
+# Restituisce la lista delle parole ordinate secondo la loro frequenza.
 def word_frequency(defs):
     frequency = {}
     for definition in defs:
@@ -27,40 +14,41 @@ def word_frequency(defs):
                 frequency[word] += 1
     return list(dict(sorted(frequency.items(), key=lambda item: item[1], reverse=True)).keys())
 
-
-def get_most_commomn(defs, frequency, threshold):
-    freqency_total = {}
+# Calcola le parole più comuni in base a threshold,
+# cioè vengono prese solo quelle parole la cui frequenza relativa è maggiore di threshold
+def get_most_common(defs, frequency, threshold):
+    frequency_total = {}
     for word in frequency:
         for definition in defs:
             if word in definition:
-                if word not in freqency_total:
-                    freqency_total[word] = 1
+                if word not in frequency_total:
+                    frequency_total[word] = 1
                 else:
-                    freqency_total[word] += 1
+                    frequency_total[word] += 1
     result = []
-    for word in freqency_total.keys():
-        def_frequency = freqency_total[word] / len(defs)
+    for word in frequency_total.keys():
+        def_frequency = frequency_total[word] / len(defs)
         if def_frequency >= threshold:
             result.append(word)
         else:
             break
     return result
 
-
+# Calcola la similarity dividendo la cardinalità delle parole più comuni per il numero totale delle definizioni
 def get_similarity(total_common_word, total_def):
     return total_common_word / total_def
 
 
 if __name__ == '__main__':
-    snow_stm = SnowballStemmer('english')
     stopword = stopwords.words('english')
     translator = str.maketrans('', '', string.punctuation)
     f = open("./TLN-definitions-24.csv", "r")
-    print(f.readline())
+    f.readline()
     pen = []
     cigarette = []
     cloud = []
     ontology = []
+    # pulizia delle definizioni in input
     for line in f.readlines():
         splitted_line = line.split('#')
         pen.append(splitted_line[1].replace('"', '').replace('\n', '').strip().lower().translate(translator))
@@ -68,35 +56,39 @@ if __name__ == '__main__':
         cloud.append(splitted_line[3].replace('"', '').replace('\n', '').strip().lower().translate(translator))
         ontology.append(splitted_line[4].replace('"', '').replace('\n', '').strip().lower().translate(translator))
 
-    threshold = 0.6
+    # threshold serve per prendere solo le parole che compaiono almeno nel (threshold * 100)% delle frasi
+    threshold = 0.5
+    print("Threshold utilizzato nell'esecuzione: ", threshold)
+    print()
+
+    print("PEN (GENERICO/CONCRETO)")
     frequency = word_frequency(pen)
-    most_frequent = get_most_commomn(pen, frequency, threshold)
+    most_frequent = get_most_common(pen, frequency, threshold)
     similarity = get_similarity(len(most_frequent), len(pen))
-    print(frequency)
-    print(most_frequent)
-    print(similarity)
+    print("intersezione delle parole più comuni: ", most_frequent)
+    print("valore di similarità: ", similarity)
     print()
 
+    print("CIGARETTE (SPECIFICO/CONCRETO)")
     frequency = word_frequency(cigarette)
-    most_frequent = get_most_commomn(cigarette, frequency, threshold)
+    most_frequent = get_most_common(cigarette, frequency, threshold)
     similarity = get_similarity(len(most_frequent), len(cigarette))
-    print(frequency)
-    print(most_frequent)
-    print(similarity)
+    print("intersezione delle parole più comuni: ", most_frequent)
+    print("valore di similarità: ", similarity)
     print()
 
+    print("CLOUD (GENERICO/ASTRATTO)")
     frequency = word_frequency(cloud)
-    most_frequent = get_most_commomn(cloud, frequency, threshold)
+    most_frequent = get_most_common(cloud, frequency, threshold)
     similarity = get_similarity(len(most_frequent), len(cloud))
-    print(frequency)
-    print(most_frequent)
-    print(similarity)
+    print("intersezione delle parole più comuni: ", most_frequent)
+    print("valore di similarità: ", similarity)
     print()
 
+    print("ONTOLOGY (SPECIFICO/ASTRATTO)")
     frequency = word_frequency(ontology)
-    most_frequent = get_most_commomn(ontology, frequency, threshold)
+    most_frequent = get_most_common(ontology, frequency, threshold)
     similarity = get_similarity(len(most_frequent), len(ontology))
-    print(frequency)
-    print(most_frequent)
-    print(similarity)
+    print("intersezione delle parole più comuni: ", most_frequent)
+    print("valore di similarità: ", similarity)
     print()
